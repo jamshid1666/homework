c6f4364a09b2d65c0bb6ca32dc104c39fc2f9e71
jamshid1666 <saribayev94@mail.ru>
2025-04-15 18:03:02 +0500

    dars js dars html

--- 7-dars/dars.js
+++ 7-dars/dars.js
@@ -12,3 +12,56 @@
 // console.log(res);
 
 // a(2)(3)(4)(5)(5)() oxirida qavs ichiga parametr berilmasa  yigindi qaytarsin
+
+// function add(a,b) {
+//     console.log(this);
+//     return a+b + this.x + this.y;
+// }
+
+// // add(1,2);
+// const obj = {
+//     x:12,
+//     y:55,
+// };
+
+// console.log(add.call(obj, 1, 2));
+
+// const person = {
+//     firstName: 'Ali',
+//     lastName: 'Valiyev',
+// };
+
+// const person2 = {
+//     firstName: 'Jamshid',
+//     lastName: 'Saribayev',
+// };
+
+// function greet (greeting) {
+//     console.log(`${greeting}, ${this.firstName} ${this.lastName}`);
+// }
+
+// greet.call(person2, 'Privet');
+
+// function greet(a,b) {
+//     return `${this.firstName}, ${a}. ${b}`;
+// }
+
+// let res = greet.apply(person2, ['Good morning', 'How are you']);
+// console.log(res);
+
+// const student1 = {
+//     names: 'Jam',
+//     grade: '5',
+//     introduction: function () {
+//         console.log(this.names + ' studies in grade ' + this.grade + '.');
+//     },
+// };
+
+// const student2 = {
+//     names: 'Javoh',
+//     grade: '6',
+// };
+
+// let res = student1.introduction.bind(student2);
+// let res2 = student1.introduction();
+// res();
